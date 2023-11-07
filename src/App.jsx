7d95aa02ddd263fa11f076f19387a0595d502e11
Isuru Eranga <isurueranga.0@gmail.com>
2023-11-07 13:37:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import GitHubIcon from '@mui/icons-material/GitHub';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import PhoneIcon from '@mui/icons-material/Phone';
+import EmailIcon from '@mui/icons-material/Email';
+
+function App() {
+
+  return (
+    <>
+    
+      <div className=' flex flex-col h-screen justify-center items-center bg-blue-100 space-y-5'>
+
+        <p className=' text-4xl md:text-6xl lg:text-6xl text-cente'>Hello, </p>
+
+        <p className=' text-5xl md:text-7xl lg:text-8xl text-center'>I am <span className=' text-blue-400'>Isuru Eranga</span></p>
+
+        <div className=' flex flex-col md:flex-row text-center text-xl md:text-2xl lg:text-3xl space-x-3'>
+          <p>Data Science Enthusiast</p>
+          <p className=' hidden md:block lg:block'>|</p>
+          <p>Full Stack Developer</p>
+          <p className=' hidden md:block lg:block'>|</p>
+          <p>UI UX Designer</p>
+        </div>
+
+        
+
+        <p className=' text-lg md:text-xl lg:text-2xl text-center'>Reading BSc (Hons) in Computing & Information Systems at Sabaragmuwa University of Sri Lanka</p>
+
+        <div className=' space-x-3 mt-7'>
+          <a href="https://github.com/isurueranga-98"><GitHubIcon/></a>
+          <a href="https://www.linkedin.com/in/isuru-eranga-230300110/"><LinkedInIcon/></a>
+          <a href="https://www.facebook.com/profile.php?id=61552382926842"><FacebookIcon/></a>
+         
+        </div>
+
+        <div className='  flex flex-col space-y-3 mt-7 justify-center items-center'>
+          <div className='  flex flex-row items-center'>
+            <PhoneIcon className=' '/>
+            <p className=' text-[18px] mx-3 font-medium'>+94762605123</p>
+          </div>
+
+          <div className='  flex flex-row items-center'>
+            <EmailIcon/>
+            <p className=' text-[18px] mx-3 font-medium'>isurueranga.0@gmail.com</p>
+          </div>
+        </div>
+        <p className=' text-lg mb-auto'>This website under maintain</p>
+        
+        
+      </div>
+   
+    </>
+  )
+}
+
+export default App
