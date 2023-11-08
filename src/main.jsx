28f52da36dd8785a41cdb2ad817a72ed3a5d9382
isurueranga-98 <58843387+isurueranga-98@users.noreply.github.com>
2023-11-08 14:22:00 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import Nav from './Nav.jsx'
+import Footer from './Footer.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Nav/>
     <App/>
+    <Footer/>
   </React.StrictMode>,
 )
