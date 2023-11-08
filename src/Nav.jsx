6f2753c158036aad3d7c6d3ea9d1071c41c5e26d
isurueranga-98 <58843387+isurueranga-98@users.noreply.github.com>
2023-11-08 15:30:08 +0530

    Update Nav.jsx

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -7,9 +7,9 @@ const Nav = () => {
     <div >
       <nav >
         <div className='flex flex-row items-center justify-between mt-10 mx-6 md:mx-20 lg:mx-20'>
-            <img src={logo} alt="sumz_logo" className=' w-20 md:w-28 lg:w-28'/> 
+            <img src={logo} alt="sumz_logo" className=' w-28'/> 
            
-            <button type=' button' className=' rounded-full border border-black bg-black py-0.5 px-2 text-sm text-white transition-all hover:bg-white hover:text-black md:py-1.5 md:px-5 lg:py-1.5 lg:px-5 ' onClick={() => window.open('https://github.com/isurueranga-98')}>
+            <button type=' button' className=' rounded-full border border-black bg-black  text-sm text-white transition-all hover:bg-white hover:text-black py-1.5 px-5 ' onClick={() => window.open('https://github.com/isurueranga-98')}>
                 <GitHubIcon/>
                
             </button>
