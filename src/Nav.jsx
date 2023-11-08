da9e43396e7e9b82d33de55433e9b84fb84e81d7
isurueranga-98 <58843387+isurueranga-98@users.noreply.github.com>
2023-11-08 09:26:23 +0530

    Add files via upload

---
+++ src/Nav.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from './assets/signature.png'
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+const Nav = () => {
+  return (
+    <div >
+      <nav >
+        <div className='flex flex-row items-center justify-between mt-10 mx-6 md:mx-20 lg:mx-20'>
+            <img src={logo} alt="sumz_logo" className=' w-28'/>
+            <button type=' button' className=' rounded-full border border-black bg-black py-1.5 px-5 text-sm text-white transition-all hover:bg-white hover:text-black' onClick={() => window.open('https://github.com/isurueranga-98')}>
+                <GitHubIcon/>
+            </button>
+        </div>
+        </nav>
+            
+        
+    </div>
+  )
+}
+
+export default Nav
