4c8b6a845da181541da5ab83b26c778a36ac040b
isurueranga-98 <58843387+isurueranga-98@users.noreply.github.com>
2023-11-08 14:21:04 +0530

    Add files via upload

---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <footer className=' flex flex-col justify-center items-center'>
+        <p className=' text-lg mb-auto'>This website under maintenance</p>
+        <p className=' text-xs mb-auto'>All Rights Reserved</p>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
