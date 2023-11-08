da3e094ba4e242f6b50c1e26fa09516f75393235
isurueranga-98 <58843387+isurueranga-98@users.noreply.github.com>
2023-11-08 15:44:34 +0530

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <div>
         <footer className=' flex flex-col justify-center items-center'>
-        <p className=' text-lg mb-auto'>This website under maintenance</p>
-        <p className=' text-xs mb-auto'>All Rights Reserved</p>
+        <p className=' text-lg mb-auto'>This Website Under Developing</p>
+        <p className=' text-xs mb-auto'>All Rights Reserved 2023</p>
         </footer>
     </div>
   )
