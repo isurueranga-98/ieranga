9dc36bb0ffdcf1b648f29d271fbc81c17f1cad5e
Isuru Eranga <58843387+isurueranga-98@users.noreply.github.com>
2023-11-13 19:34:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,18 @@ import App from './App.jsx'
 import './index.css'
 import Nav from './Nav.jsx'
 import Footer from './Footer.jsx'
+import Aboutme from './Aboutme.jsx'
+import Project from './Project.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Nav/>
-    <App/>
-    <Footer/>
+    <div>
+      <Nav/>
+      <App/>
+      <Aboutme/>
+      {/* <Project/> */}
+      <Footer/>
+      
+    </div>
   </React.StrictMode>,
 )
