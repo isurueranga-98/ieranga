31cc0ca02d4985109b2b7de587fe6dc6d0473140
Isuru Eranga <isurueranga.0@gmail.com>
2024-09-06 16:01:22 +0530

    new

--- components/intro.tsx
+++ components/intro.tsx
@@ -75,7 +75,7 @@ export default function Intro() {
 
               <a
                 className="group bg-white px-7 py-3 flex items-center justify-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 text-sm"
-                href="/SE_Isuru Eranga.pdf"
+                href="/Isuru-Eranga-UI UXpdf"
                 download
               >
                 Download CV{" "}
